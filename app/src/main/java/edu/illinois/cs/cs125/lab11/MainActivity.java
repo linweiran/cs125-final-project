4060f059c71e826197919a077145b7d093c56c86
wlin40 <wlin40@illinois.edu>
2018-12-02 16:14:50 -0600

    initial

---
+++ app/src/main/java/edu/illinois/cs/cs125/lab11/MainActivity.java
@@ -0,0 +1,220 @@
+package edu.illinois.cs.cs125.lab11;
+
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Main class for our UI design lab.
+ */
+public final class MainActivity extends AppCompatActivity {
+    /** Default logging tag for messages from the main activity. */
+    private static final String TAG = "Lab11:Main";
+
+    /** Request queue for our API requests. */
+    private static RequestQueue requestQueue;
+
+    /**
+     * Run when this activity comes to the foreground.
+     *
+     * @param savedInstanceState unused
+     */
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set up the queue for our API requests
+        requestQueue = Volley.newRequestQueue(this);
+
+        setContentView(R.layout.activity_main);
+
+        //startAPICall("192.17.96.8");
+        //startAPICall();
+        Button btn = findViewById(R.id.getweather);
+        final EditText toinput = findViewById(R.id.inputedit);
+        final EditText toinput2 = findViewById(R.id.inputedit2);
+        final EditText toinput3 = findViewById(R.id.inputedit3);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] li = new String[153];
+
+                int a = toinput.getText().toString().hashCode();
+                int b = toinput2.getText().toString().hashCode();
+                int c = toinput3.getText().toString().hashCode();
+                if (a < 0) a = -a;
+                if (b < 0) b = -b;
+                if (c < 0) c = -c;
+                a = a % 150;
+                b = b % 150;
+                c = c % 150;
+                if (a == b) b++;
+                if (b == c) c++;
+                startAPICall("192.17.96.8",a,b,c);
+                //Toast.makeText(MainActivity.this, Integer.toString(a)+Integer.toString(b)+Integer.toString(c), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    /**
+     * Run when this activity is no longer visible.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    /**
+     * Make a call to the IP geolocation API.
+     *
+     *ipAddress IP address to look up
+     */
+    void startAPICall(final String ipAddress, final int a, final int b, final int c) {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://dog.ceo/api/breeds/list/all",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            //apiCallDone(response);
+                            Log.d(TAG, response.toString());
+                            String k = response.toString();
+                            String aa="dakfjadkj";
+                            String bb="uqgiuGUI";
+                            String cc="SIUHRIOSUH";
+                            String l="";
+                            int count = 0;
+                            boolean x = false;
+                            int i = 0;
+                            while (i < k.length()) {
+                                if (k.charAt(i) == '"') {
+                                    if (x == false) {
+                                        x= true;
+                                    }   else {
+                                        Log.d(TAG, l);
+                                        if (count+3 == a) aa=l;
+                                        if (count+3 == b) bb=l;
+                                        if (count+3 == c) cc=l;
+                                        l = "";
+
+                                        count++;
+                                        x = false;
+                                    }
+                                    i++;
+                                    continue;
+                                }
+                                if (x) {
+                                    l += k.charAt(i);
+                                }
+                                i++;
+                            }
+                            Log.d(TAG,Integer.toString(count));
+                            Toast.makeText(MainActivity.this, aa+", "+bb+", "+cc, Toast.LENGTH_SHORT).show();
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(final VolleyError error) {
+                            Log.e(TAG, error.toString());
+                        }
+                    });
+            jsonObjectRequest.setShouldCache(false);
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+   /* void startAPICall() {
+        final String[] k = new String[1];
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+
+                    Request.Method.GET,
+                    "https://dog.ceo/api/breeds/list/all",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            Log.d(TAG, response.toString());
+                            k[0] = response.toString();
+                            Log.d(TAG, "tag");
+                            Log.d(TAG, k[0]);
+
+                            String l="";
+                            int count = 0;
+                            boolean x = false;
+                            int i = 0;
+                            while (i < k[0].length()) {
+                                if (k[0].charAt(i) == '"') {
+                                    if (x == false) {
+                                        x= true;
+                                    }   else {
+                                        Log.d(TAG, l);
+                                        l = "";
+                                        count++;
+                                        x = false;
+                                    }
+                                    i++;
+                                    continue;
+                                }
+                                if (x) {
+                                    l += k[0].charAt(i);
+                                }
+                                i++;
+                            }
+                            Log.d(TAG,Integer.toString(count));
+
+
+
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+            requestQueue.getCache();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    } */
+
+
+
+
+    /**
+     * Handle the response from our IP geolocation API.
+     *
+     * @param response response from our IP geolocation API.
+     */
+    void apiCallDone(final JSONObject response) {
+        try {
+            Log.d(TAG, response.toString(2));
+            // Example of how to pull a field off the returned JSON object
+            Log.i(TAG, response.get("hostname").toString());
+        } catch (JSONException ignored) { }
+    }
+}
